8ab83edb40a76c4148933184c5e7ddb2c90eaf95
Asselya Moldasheva <a.moldasheva@gmail.com>
2019-06-27 12:50:23 +0600

    Добавьте файлы проекта.

---
+++ CW_1.3_25.06/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+//3.	Дано расстояние в сантиметрах. Найти число полных метров в нем.
+
+namespace CW_1._3_25._06
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sm, m;
+            Console.WriteLine("Введите расстояние в сантиметрах:");
+            string textAsNum = Console.ReadLine();
+            sm = int.Parse(textAsNum);
+            m = sm/100;
+            Console.Write("Число полных метров: ");
+            Console.WriteLine(m);
+            Console.ReadKey();
+        }
+    }
+}
